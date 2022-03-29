165c29023bef49438072eac6449ce6d7194770b0
zhangtuo <13120409722@163.com>
2022-03-29 16:22:38 +0800

    004-005 完成非路由组件 Header 和 Footer

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,22 @@
 <template>
   <div id="app">
-
+    <Header></Header>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default {
   name: 'App',
-
+  components: {
+    Header,Footer
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
