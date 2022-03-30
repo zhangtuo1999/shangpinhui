2b160f391559ade623370ac4ff305d1ee333a82b
zhangtuo <13120409722@163.com>
2022-03-30 22:48:06 +0800

    011 - 013 完成主页相关组件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '@/router'
+import TypeNav from "@/pages/Home/TypeNav";
+
+// 注册全局组件
+Vue.component(TypeNav.name, TypeNav)
 
 Vue.config.productionTip = false
 
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
-    首页
+    <type-nav></type-nav>
+    <list-container></list-container>
+    <today-recommend></today-recommend>
+    <rank></rank>
+    <like></like>
+    <floor></floor>
+    <floor></floor>
+    <bland></bland>
   </div>
 </template>
 
 <script>
+import ListContainer from "@/pages/Home/ListContainer";
+import TodayRecommend from "@/pages/Home/TodayRecommend";
+import Rank from "@/pages/Home/Rank";
+import Like from "@/pages/Home/Like";
+import Floor from "@/pages/Home/Floor";
+import Bland from "@/pages/Home/Bland";
+
 export default {
-  name: "index"
+  name: "index",
+  components: {Bland, Floor, Like, Rank, TodayRecommend, ListContainer},
 }
 </script>
 
